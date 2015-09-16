34a358c039d671d45667e1ae3dccffc76fc49b05
Kobnar <konradicus.ludwidicus.maximus@gmail.com>
2015-09-16 14:53:19 -0700

    Fix bad traversal hookup.

--- ncmdb/__init__.py
+++ ncmdb/__init__.py
@@ -8,9 +8,10 @@ from .models import DBSession, Base
 
 def traversal_factory(request):
     root = RootResource(None, '')
-    root['api'] = RootResource(root, 'api')
-    root['api']['v1'] = RootResource(root['api'], 'v1')
-    root['api']['v1']['people'] = PersonTableResource(root['api']['v1'], 'people')
+    root['api'] = RootResource
+    root['api']['v1'] = RootResource
+    root['api']['v1']['people'] = PersonTableResource
+    return root
 
 
 def main(global_config, **settings):
@@ -24,7 +25,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
     config.add_renderer('json', JSON(indent=4))
     config.scan()
     return config.make_wsgi_app()
