bb31f9f4f9e709ba649b03c54ee6936e332b62f5
Kobnar <konradicus.ludwidicus.maximus@gmail.com>
2015-09-16 14:02:55 -0700

    Add traversal factory to __init__.

--- ncmdb/__init__.py
+++ ncmdb/__init__.py
@@ -1,7 +1,15 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
+from .resources import RootResource, PersonRowResource, PersonTableResource
 from .models import DBSession, Base
+
+
+def traversal_factory(request):
+    root = RootResource(None, '')
+    root['api'] = RootResource(root, 'api')
+    root['api']['v1'] = RootResource(root['api'], 'v1')
+    root['api']['v1']['people'] = PersonTableResource(root['api']['v1'], 'people')
 
 
 def main(global_config, **settings):
@@ -10,7 +18,8 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+    config = Configurator(settings=settings,
+                          root_factory=traversal_factory)
     config.include('pyramid_jinja2')
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
